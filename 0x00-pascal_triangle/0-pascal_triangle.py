396f1b83ba3686c0a061e499b965e8f460e304c0
Mubarak1A <adesinamubarak123@example.com>
2023-07-31 12:57:42 +0100

    add README.md and check pep8

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Module of pascal_triangle function"""
+
 def pascal_triangle(n):
     """returns a list of lists of integers representing the Pascal’s triangle of n:
        Returns an empty list if n <= 0
